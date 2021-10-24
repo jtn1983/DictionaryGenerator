c01194fadf376b8757e4dd8975b9d48d36bc6de2
Yakov Tenilin <yakov@tenilin.ru>
2021-10-24 23:21:02 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,23 @@
+import java.util.Collection;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)){
+            System.out.println("Input text:");
+            String text = scanner.nextLine();
+            splitText(text);
+
+        }
+    }
+
+
+    public static void splitText(String text) {
+        Stream.of(text.split("[^A-Za-zА-Яа-я]+"))
+                .map(String::toLowerCase)
+                .sorted()
+                .forEach(System.out::println);
+    }
+}
